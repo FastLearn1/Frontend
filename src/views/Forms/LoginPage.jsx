a045c1e1ab2826f0da0eb2f75465709d0fe10551
Ayelen <ayelen.celis92@gmail.com>
2024-07-14 17:33:07 -0500

    first commit

---
+++ src/views/Forms/LoginPage.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import { Helmet } from "react-helmet-async";
+import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+import axios from "axios";
+import { SectionForm } from "../../components/";
+
+const URL = import.meta.env.VITE_BACKEND_URL;
+
+const LoginPage = () => {
+  const [initialFields] = useState([
+    { type: "email", placeholder: "Correo electrónico", id: "correo" },
+    { type: "password", placeholder: "Contraseña", id: "contrasena" },
+  ]);
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (formData) => {
+    try {
+      const response = await axios.post(
+        `${URL}/login`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      console.log("Respuesta del servidor:", response.data);
+
+      const { token } = response.data;
+      if (token) {
+        localStorage.setItem("token", token);
+
+        const decodedToken = jwtDecode(token);
+
+        console.log("Token decodificado", decodedToken);
+
+        const role = decodedToken.rol
+
+        if (role === "student") {
+          navigate("/student");
+        } else if (role === "teacher") {
+          navigate("/teacher");
+        } else if (role === "admin") {
+          navigate("/admin");
+        } else {
+          navigate("/login");
+        }
+      }
+    } catch (error) {
+      console.error("Error al iniciar sesión:", error);
+    }
+  };
+
+  return (
+    <>
+      <Helmet>
+        <title>Iniciar sesión | FastLearn</title>
+      </Helmet>
+
+      <main
+        className="h-screen bg-center bg-cover flex justify-center items-center"
+        style={{ backgroundImage: "url(/img/forms/img2.webp)" }}
+      >
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+        <SectionForm
+          title="Inicia sesión en tu cuenta"
+          text="Introduce tus credenciales para acceder a tu cuenta."
+          textRecovery="Olvidé mi contraseña"
+          linkRecovery="/recovery"
+          formFields={initialFields}
+          linkText="No tengo una cuenta"
+          linkHref="/signup"
+          onSubmit={handleLogin}
+          formType="login"
+        />
+      </main>
+    </>
+  );
+};
+
+export default LoginPage;
---
+++ src/views/Home/ArticlesPage.jsx
@@ -0,0 +1,84 @@
+import { Helmet } from "react-helmet-async";
+import { Header, Footer } from "../../components/";
+
+const ArticlesPage = () => {
+  const headerImages = [
+    "/img/header/SectionArticle1.webp",
+    "/img/header/SectionArticle2.webp",
+  ];
+
+  const navItems = [
+    { href: "/", label: "Inicio" },
+    { href: "/explore", label: "Explorar" },
+    { href: "/signup", label: "Crear cuenta" },
+    { href: "/support", label: "Soporte" },
+  ];
+
+  const headerProps = {
+    titleHero: "Bienvenido a la sección de articulos",
+    textHero: "Estos son los articulos más importantes de la semana",
+    btn1Hero: {
+      label: "Ver articulos",
+      href: "#",
+    },
+    btn2Hero: {
+      label: "Mostrar todos",
+      href: "/all-articles",
+    },
+  };
+
+  const services = {
+    title: "Información",
+    links: [
+      { text: "Explorar", href: "/explore" },
+      { text: "Articulos", href: "/articles" },
+      { text: "Cursos", href: "/courses" },
+      { text: "Soporte", href: "/support" },
+      { text: "FAQS", href: "/faqs" },
+      { text: "Reportar un problema", href: "/report" },
+    ],
+  };
+
+  const company = {
+    title: "Compañia",
+    links: [
+      { text: "Documentación", href: "/docs" },
+      { text: "Manual de usuario", href: "/manual-usewr" },
+      { text: "Manual técnico", href: "/manual-tech" },
+    ],
+  };
+
+  const legal = {
+    title: "Legal",
+    links: [
+      { text: "Términos y condiciones", href: "/terms" },
+      { text: "Política de privacidad", href: "/policy" },
+      { text: "Derechos de autor", href: "/rights-autor" },
+    ],
+  };
+
+  const companyName = "FastLearn INC";
+  const companyDescription = "Todos los derechos reservados";
+  return (
+    <>
+      <Helmet>
+        <title>Articulos de FastLearn</title>
+      </Helmet>
+      <Header
+        images={headerImages}
+        interval={5000}
+        heroProps={headerProps}
+        menuItems={navItems}
+      />
+      <Footer
+        services={services}
+        company={company}
+        legal={legal}
+        companyName={companyName}
+        companyDescription={companyDescription}
+      />
+    </>
+  );
+};
+
+export default ArticlesPage;
